15a5071b6dab44043c1b58049b5f104c2c208dd9
goshenkata <goshenkataklev@gmail.com>
2021-11-08 14:51:55 +0200

    did exercise 7

--- src/com/AssociativeArraysLambdaStreamAPI/Exercise/CompanyUsers/Main.java
+++ src/com/AssociativeArraysLambdaStreamAPI/Exercise/CompanyUsers/Main.java
@@ -1,4 +1,32 @@
 package com.AssociativeArraysLambdaStreamAPI.Exercise.CompanyUsers;
 
+import java.util.*;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Map<String, Set<String>> map = new LinkedHashMap<>();
+        String input = scanner.nextLine();
+        while (!input.equals("End")) {
+            String company = input.split(" -> ")[0];
+            String employeeID = input.split(" -> ")[1];
+            if (!map.containsKey(company)) {
+                map.put(company, new LinkedHashSet<>());
+            }
+            if (!map.get(company).contains(employeeID)) {
+                map.get(company).add(employeeID);
+            }
+
+            input = scanner.nextLine();
+        }
+        map.entrySet().stream()
+                .sorted(Map.Entry.<String, Set<String>>comparingByKey())
+                .forEach(e -> {
+                    System.out.println(e.getKey());
+                    e.getValue().forEach(s -> System.out.println("-- " + s));
+                });
+
+    }
+
+
 }
