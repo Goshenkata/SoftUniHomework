e0cea14aadf8535270e21b23683ed5315981fa65
goshenkata <goshenkataklev@gmail.com>
2021-11-11 13:53:49 +0200

    test

---
+++ src/com/TextProcessing/lab/TextFilter/Main.java
@@ -0,0 +1,9 @@
+package com.TextProcessing.lab.TextFilter;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
+}
