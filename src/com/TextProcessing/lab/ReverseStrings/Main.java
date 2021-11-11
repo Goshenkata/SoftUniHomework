7fb13d8ed7b8be8587d0ababb0f4ba9d9cbef90b
goshenkata <goshnekataklev@gmail.com>
2021-11-11 13:06:13 +0200

    did exercise 1

---
+++ src/com/TextProcessing/lab/ReverseStrings/Main.java
@@ -0,0 +1,18 @@
+package com.TextProcessing.lab.ReverseStrings;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        while (!input.equals("end")) {
+            StringBuilder reversed = new StringBuilder();
+            for (int i = input.length() - 1; i >= 0; i--) {
+                reversed.append(input.charAt(i));
+            }
+            System.out.printf("%s = %s%n", input, reversed);
+            input = scanner.nextLine();
+        }
+    }
+}
