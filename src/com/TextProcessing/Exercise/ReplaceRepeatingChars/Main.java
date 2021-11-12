412be5762381e7ca904fb36981dbcbb860e16b7b
goshenkata <goshenkataklev@gmail.com>
2021-11-13 00:13:16 +0200

    did exercise 6

--- src/com/TextProcessing/Exercise/ReplaceRepeatingChars/Main.java
+++ src/com/TextProcessing/Exercise/ReplaceRepeatingChars/Main.java
@@ -8,7 +8,7 @@ public class Main {
         String input = scanner.nextLine();
         StringBuilder output = new StringBuilder();
         for (int i = 0; i < input.length(); i++) {
-            while (input.charAt(i) == input.charAt(i + 1) && i <= input.length()-2) {
+            while (i <= input.length() - 2 && input.charAt(i) == input.charAt(i + 1)) {
                 i++;
             }
             output.append(input.charAt(i));
