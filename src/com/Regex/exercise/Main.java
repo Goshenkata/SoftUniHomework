fdfe157b785b01b232752ac8abd2b3f0d9f0b791
Goshenkata <goshenkataklev@gmail.com>
2021-11-19 18:38:30 +0200

    did exercise 1

---
+++ src/com/Regex/exercise/Main.java
@@ -0,0 +1,34 @@
+package com.Regex.exercise;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        List<String> furnitures = new ArrayList<>();
+        double total = 0;
+        Pattern pattern = Pattern.compile(">>(?<furniture>[A-Za-z]+)<<(?<price>[\\d]+.*[\\d]*)!(?<quantity>\\d*)");
+        while (!input.equals("Purchase")) {
+            Matcher matcher = pattern.matcher(input);
+            if (matcher.find()) {
+                String furniture = matcher.group("furniture");
+                double price = Double.parseDouble(matcher.group("price"));
+                int quantity = Integer.parseInt(matcher.group("quantity"));
+                total += price*quantity;
+                furnitures.add(furniture);
+            }
+            input = scanner.nextLine();
+        }
+        System.out.println("Bought furniture:");
+        furnitures.forEach(System.out::println);
+        System.out.printf("Total money spend: %.2f", total);
+    }
+
+}
+    
