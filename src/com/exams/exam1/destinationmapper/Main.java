33630dbf0dde9cbc56a7c846243e8e1473ee9d1a
Goshenkata <goshenkataklev@gmail.com>
2021-11-28 22:25:44 +0200

    did exerice 2

--- src/com/exams/exam1/destinationmapper/Main.java
+++ src/com/exams/exam1/destinationmapper/Main.java
@@ -1,9 +1,27 @@
 package com.exams.exam1.destinationmapper;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Main {
 
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        String locations = scanner.nextLine();
+        Pattern pattern = Pattern.compile("([=\\/])(?<destination>[A-Z][A-Za-z]{2,})\\1");
+        Matcher matcher = pattern.matcher(locations);
+        int travelPoints = 0;
+        List<String> destinations = new ArrayList<>();
+        while (matcher.find()) {
+            String destination = matcher.group("destination");
+            travelPoints += destination.length();
+            destinations.add(destination);
+        }
+        System.out.printf("Destinations: %s%n", String.join(", ", destinations));
+        System.out.printf("Travel Points: %d", travelPoints);
     }
 
 }
