1fe7c8702be950e7c8d2a03489b71c6436f20d7e
Goshenkata <goshenkataklev@gmail.com>
2021-12-03 21:07:57 +0200

    did exercise 1

--- src/com/AssociativeArraysLambdaStreamAPI/MoreExrcise/Ranking/Main.java
+++ src/com/AssociativeArraysLambdaStreamAPI/MoreExrcise/Ranking/Main.java
@@ -1,56 +1,62 @@
 package com.AssociativeArraysLambdaStreamAPI.MoreExrcise.Ranking;
 
+
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Map<String, String> contests = new LinkedHashMap<>();
-        Map<String, List<User>> submissions = new LinkedHashMap<>();
         String input = scanner.nextLine();
+        Map<String, Map<String, Integer>> submissions = new LinkedHashMap<>();
         while (!input.equals("end of contests")) {
-            String[] split = input.split(":");
-            contests.put(split[0], split[1]);
-            submissions.put(split[0], new ArrayList<>());
+            contests.put(input.split(":")[0], input.split(":")[1]);
+            input = scanner.nextLine();
         }
         input = scanner.nextLine();
-        while (input.equals("end of contests")) {
+        while (!input.equals("end of submissions")) {
             String[] split = input.split("=>");
             String contest = split[0];
             String password = split[1];
             String username = split[2];
             int points = Integer.parseInt(split[3]);
-            if (contests.get(contest).equals(password)) {
+            if (contests.containsKey(contest)) {
+                if (contests.get(contest).equals(password)) {
+                    if (submissions.containsKey(username)) {
+                        if (submissions.get(username).containsKey(contest)) {
+                            if (submissions.get(username).get(contest) < points) {
+                                submissions.get(username).put(contest, points);
+                            }
+                        } else submissions.get(username).put(contest, points);
+                    } else {
+                        submissions.put(username, new LinkedHashMap<>());
+                        submissions.get(username).put(contest, points);
+                    }
+                }
             }
             input = scanner.nextLine();
         }
-    }
-
-    static class User {
-        String username;
-        String password;
-
-        public String getUsername() {
-            return username;
+        int maxPoints = Integer.MIN_VALUE;
+        String maxPointStudent = "";
+        for (Map.Entry<String, Map<String, Integer>> o : submissions.entrySet()) {
+            int sum = o.getValue().values().stream().mapToInt(Integer::intValue).sum();
+            if (maxPoints < sum) {
+                maxPoints = sum;
+                maxPointStudent = o.getKey();
+            }
         }
-
-        public User setUsername(String username) {
-            this.username = username;
-            return this;
-        }
-
-        public String getPassword() {
-            return password;
-        }
-
-        public User setPassword(String password) {
-            this.password = password;
-            return this;
-        }
-
-        public User(String username, String password) {
-            this.username = username;
-            this.password = password;
-        }
+        System.out.printf("Best candidate is %s with total %d points.%n", maxPointStudent, maxPoints);
+        System.out.println("Ranking:");
+        //At the end you have to print the info for the user with the most points in the format "Best candidate is {user} with total {total points} points.
+        // ". After that print all students ordered by their names.
+        // For each user print each contest with the points in descending order. See the examples.
+        submissions.entrySet().stream()
+                .sorted(Map.Entry.comparingByKey())
+                .forEach(k -> {
+                    System.out.println(k.getKey());
+                    k.getValue().entrySet().stream()
+                            .sorted(Map.Entry.<String, Integer>comparingByValue().reversed())
+                            .forEach(kv -> System.out.printf("#  %s -> %d%n", kv.getKey(), kv.getValue()));
+                });
     }
 }
