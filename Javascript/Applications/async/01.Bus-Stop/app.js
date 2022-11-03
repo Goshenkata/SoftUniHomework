7e8a7d2f63f33e9546c32d903e0ee01144e718a7
Goshenkata <Koleto24@protonmail.com>
2022-11-03 22:21:13 +0200

    more

---
+++ Javascript/Applications/async/01.Bus-Stop/app.js
@@ -0,0 +1,21 @@
+async function getInfo() {
+  let id = Number(document.getElementById("stopId").value);
+  let ul = document.getElementById("buses");
+  try {
+    const response = await fetch(
+      `http://localhost:3030/jsonstore/bus/businfo/${id}`
+    );
+    const data = await response.json();
+    document.getElementById("stopName").textContent = data.name;
+    for (const busId in data.buses) {
+      if (Object.hasOwnProperty.call(data.buses, busId)) {
+        const time = data.buses[busId];
+        let li = document.createElement("li");
+        li.textContent = `Bus ${busId} arrives in ${time} minutes`
+				ul.appendChild(li)
+      }
+    }
+  } catch (error) {
+    document.getElementById("stopName").textContent = 'Error'
+	}
+}
